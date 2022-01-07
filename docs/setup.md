4c9026d923624fd1445de68909f2e421e7c41379
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2022-01-07 21:03:30 +0800

    Installation using Intel xe_2018, docusaurus updated, KaTeX stylesheet updated.

--- docs/setup.md
+++ docs/setup.md
@@ -40,6 +40,30 @@ FC = mpif90 -O3 -qopenmp -I${MKLROOT}/include/fftw
 LIB= -L${MKLROOT}/include/fftw -lfftw3 -L$MKLROOT/lib/intel64 -lmkl_blacs_intelmpi_lp64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lifcore
 ```
 
+:::warning
+
+Currently I found some issues with the Intel license server while using
+`xe_2015` module at NUS HPC clusters. After the module load, you need to export
+following `ENV` variable:
+```bash
+export "INTEL_LICENSE_FILE=/app1/centos6.3/Intel/xe_2015/composer_xe_2015.3.187/licenses"
+```
+
+:::
+
+If you have `xe_2018` (instead of `xe_2015`):
+```bash
+module load xe_2018
+```
+
+Use following flags:
+```bash
+MKLROOT = /opt/intel/mkl
+CC = mpiicc -O3 -xHOST -ip -no-prec-div -qopenmp -I${MKLROOT}/include/fftw -I${MKLROOT}/include
+FC = mpiifort -O3 -xHOST -ip -no-prec-div -qopenmp
+LIB= -L${MKLROOT}/include/fftw -lfftw3 -L$MKLROOT/lib/intel64 -lmkl_blacs_intelmpi_lp64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lifcore
+```
+
 Once you are set, compile and build the executables:
 ```bash
 make all -j8
@@ -63,6 +87,13 @@ Optionally you may add the `openmx3.9/work` PATH to your `.bashrc`.
 ```bash
 export PATH="/home/svu/{username}/openmx3.9/work:$PATH"
 ```
+
+:::warning
+
+Although I was able to compile the code using above flags, I do not have
+expertise to assure this was the optimal installation.
+
+:::
 
 Here is a sample PBS job-script for NUS HPC cluster:
 ```bash
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,147 +1,158 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 const math = require("remark-math");
-const katex = require("rehype-katex");
-module.exports = {
-  title: "OpenMX tutorial",
-  tagline: "DFT calculation using OpenMX",
-  url: "https://pranabdas.github.io",
-  baseUrl: "/openmx/", // must have a trailing "/"
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
-  organizationName: "pranabdas", // Usually your GitHub org/user name.
-  projectName: "openmx", // Usually your repo name.
-  themeConfig: {
-    prism: {
-      theme: require("prism-react-renderer/themes/github"),
-      darkTheme: require("prism-react-renderer/themes/nightOwl"),
-      additionalLanguages: ["docker", "fortran"],
-    },
-    hideableSidebar: true,
-    colorMode: {
-    //   defaultMode: "dark",
-    //   switchConfig: {
-    //     // darkIcon: '☾',
-    //     // lightIcon: "☀️"
-    //   },
-    //   respectPrefersColorScheme: true,
-    },
-    navbar: {
-      title: "OpenMX",
-      hideOnScroll: true,
-      logo: {
-        alt: "Logo",
-        src: "img/favicon.ico",
-      },
-      items: [
-        {
-          to: "/",
-          label: "Docs",
-          position: "left",
+/** @type {import('@docusaurus/types').Config} */
+async function config() {
+  const katex = (await import("rehype-katex")).default;
+  return {
+    title: "OpenMX tutorial",
+    tagline: "DFT calculation using OpenMX",
+    url: "https://pranabdas.github.io",
+    baseUrl: "/openmx/", // must have a trailing "/"
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
+    organizationName: "pranabdas", // Usually your GitHub org/user name.
+    projectName: "openmx", // Usually your repo name.
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+      ({
+        prism: {
+          theme: require("prism-react-renderer/themes/github"),
+          darkTheme: require("prism-react-renderer/themes/nightOwl"),
+          additionalLanguages: ["docker", "fortran"],
+        },
+        hideableSidebar: true,
+        colorMode: {
+          //   defaultMode: "dark",
+          //   switchConfig: {
+          //     // darkIcon: '☾',
+          //     // lightIcon: "☀️"
+          //   },
+          //   respectPrefersColorScheme: true,
+        },
+        navbar: {
+          title: "OpenMX",
+          hideOnScroll: true,
+          logo: {
+            alt: "Logo",
+            src: "img/favicon.ico",
+          },
           items: [
             {
-              label: "OpenMX tutorial",
               to: "/",
+              label: "Docs",
+              position: "left",
+              items: [
+                {
+                  label: "OpenMX tutorial",
+                  to: "/",
+                  target: "_self",
+                },
+                {
+                  label: "~ ~ ~ Other Docs ~ ~ ~",
+                  to: "https://pranabdas.github.io/docs/",
+                  target: "_self",
+                },
+                {
+                  label: "ARPES Python Tools",
+                  to: "https://pranabdas.github.io/arpespythontools/",
+                  target: "_self",
+                },
+                {
+                  label: "Fortran Programming",
+                  to: "https://pranabdas.github.io/fortran/",
+                  target: "_self",
+                },
+                {
+                  label: "Linux Tutorial",
+                  to: "https://pranabdas.github.io/linux/",
+                  target: "_self",
+                },
+                {
+                  label: "Machine Learning Notes",
+                  to: "https://pranabdas.github.io/machine-learning/",
+                  target: "_self",
+                },
+                {
+                  label: "Python Tutorial",
+                  to: "https://pranabdas.github.io/python-tutorial/",
+                  target: "_self",
+                },
+                {
+                  label: "Quantum Espresso Tutorial",
+                  to: "https://pranabdas.github.io/espresso/",
+                  target: "_self",
+                },
+                {
+                  label: "SUV Python Tools",
+                  to: "https://pranabdas.github.io/suvtools/",
+                  target: "_self",
+                },
+                {
+                  label: "Condensed Matter Notes",
+                  href: "https://pranabdas.github.io/docs/condmat-notes/",
+                },
+                {
+                  label: "Javascript Tutorial",
+                  href: "https://pranabdas.github.io/docs/js-learning/",
+                },
+                {
+                  label: "SUV ARPES Manual",
+                  href: "https://pranabdas.github.io/docs/arpes-man/",
+                },
+              ],
+            },
+            {
+              to: "https://pranabdas.github.io",
+              label: "About me",
+              position: "left",
               target: "_self",
             },
             {
-              label: "~ ~ ~ Other Docs ~ ~ ~",
-              to: "https://pranabdas.github.io/docs/",
-              target: "_self",
-            },
-            {
-              label: "ARPES Python Tools",
-              to: "https://pranabdas.github.io/arpespythontools/",
-              target: "_self",
-            },
-            {
-              label: "Fortran Programming",
-              to: "https://pranabdas.github.io/fortran/",
-              target: "_self",
-            },
-            {
-              label: "Linux Tutorial",
-              to: "https://pranabdas.github.io/linux/",
-              target: "_self",
-            },
-            {
-              label: "Machine Learning Notes",
-              to: "https://pranabdas.github.io/machine-learning/",
-              target: "_self",
-            },
-            {
-              label: "Python Tutorial",
-              to: "https://pranabdas.github.io/python-tutorial/",
-              target: "_self",
-            },
-            {
-              label: "Quantum Espresso Tutorial",
-              to: "https://pranabdas.github.io/espresso/",
-              target: "_self",
-            },
-            {
-              label: "SUV Python Tools",
-              to: "https://pranabdas.github.io/suvtools/",
-              target: "_self",
-            },
-            {
-              label: "Condensed Matter Notes",
-              href: "https://pranabdas.github.io/docs/condmat-notes/",
-            },
-            {
-              label: "Javascript Tutorial",
-              href: "https://pranabdas.github.io/docs/js-learning/",
-            },
-            {
-              label: "SUV ARPES Manual",
-              href: "https://pranabdas.github.io/docs/arpes-suv-man/",
+              href: "https://github.com/pranabdas/openmx",
+              position: "right",
+              className: "header-github-link",
+              "aria-label": "GitHub repository",
             },
           ],
         },
-        {
-          to: "https://pranabdas.github.io",
-          label: "About me",
-          position: "left",
-          target: "_self",
-        },
-        {
-          href: "https://github.com/pranabdas/openmx",
-          position: "right",
-          className: "header-github-link",
-          "aria-label": "GitHub repository",
-        },
-      ],
-    },
-    footer: {
-      style: "dark",
-      copyright: `Copyright © ${new Date().getFullYear()} Pranab Das. All rights
+        footer: {
+          style: "dark",
+          copyright: `Copyright © ${new Date().getFullYear()} Pranab Das. All rights
       reserved.`,
-    },
-  },
-  presets: [
-    [
-      "@docusaurus/preset-classic",
+        },
+      }),
+
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            routeBasePath: "/",
+            // Please change this to your repo.
+            editUrl: "https://github.com/pranabdas/openmx/blob/master/",
+            remarkPlugins: [math],
+            rehypePlugins: [katex],
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    stylesheets: [
       {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
-          // Please change this to your repo.
-          editUrl: "https://github.com/pranabdas/openmx/blob/master/",
-          remarkPlugins: [math],
-          rehypePlugins: [katex],
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
+        href: "https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css",
+        type: "text/css",
+        integrity:
+          "sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ",
+        crossorigin: "anonymous",
       },
     ],
-  ],
-  stylesheets: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.13.18/dist/katex.min.css",
-      type: "text/css",
-      integrity: "sha384-zTROYFVGOfTw7JV7KUu8udsvW2fx4lWOsCEDqhBreBwlHI4ioVRtmIvEThzJHGET",
-      crossorigin: "anonymous",
-    },
-  ],
-};
+  };
+}
+
+module.exports = config;
